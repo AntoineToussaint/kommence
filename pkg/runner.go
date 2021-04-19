1e503c8fbe1c160290b57788ed07ead70fab9070
Antoine Toussaint <antoine@infima.io>
2021-04-19 10:13:43 -0400

    [IMPR] Restarting: same issue.

--- pkg/runner.go
+++ pkg/runner.go
@@ -2,11 +2,14 @@ package pkg
 
 import (
 	"context"
+	"fmt"
+	"github.com/radovskyb/watcher"
 	"io"
 	"log"
 	"os/exec"
 	"strings"
 	"sync"
+	"time"
 )
 
 
@@ -41,8 +44,64 @@ func (r *Runner) Produce(ctx context.Context) <-chan Message {
 	return r.out
 }
 
+func (r *Runner) CreateWatcher() <-chan bool {
+	out := make(chan bool)
+	w := watcher.New()
+
+	// SetMaxEvents to 1 to allow at most 1 event's to be received
+	// on the Event channel per watching cycle.
+	//
+	// If SetMaxEvents is not set, the default is to send all events.
+	w.SetMaxEvents(1)
+
+	// Only notify rename and move events.
+	w.FilterOps(watcher.Write)
+
+	// Only files that match the regular expression during file listings
+	// will be watched.
+	//r := regexp.MustCompile("^abc$")
+	//w.AddFilterHook(watcher.RegexFilterHook(r, false))
+
+	go func() {
+		for {
+			select {
+			case <-w.Event:
+				out <- true
+			case err := <-w.Error:
+				log.Fatalln(err)
+			case <-w.Closed:
+				return
+			}
+		}
+	}()
+
+	// Watch this folder for changes.
+	for _, p := range r.Watch {
+		if err := w.AddRecursive(p); err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	go func() {
+		// Start the watching process - it'll check for changes every 100ms.
+		if err := w.Start(time.Millisecond * 100); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+	return out
+}
+
 func (r *Runner) Start(ctx context.Context) {
 	go r.Run(ctx)
+	// Watcher
+	if w := r.CreateWatcher(); w != nil {
+		for {
+			select {
+			case <-w:
+				r.Restart(ctx)
+			}
+		}
+	}
 
 }
 
@@ -74,6 +133,7 @@ func (r *Runner) Restart(ctx context.Context) {
 	if err := r.cmd.Process.Kill(); err != nil {
 		log.Fatalf("failed to kill process %v: %v", r.Name, err)
 	}
+	fmt.Println("restarting:", r.Name)
 	go r.Start(ctx)
 }
 
