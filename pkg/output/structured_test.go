9a796fd05922762716b1768300ce2e31c26cce21
Antoine Toussaint <antoine@infima.io>
2021-09-09 15:19:59 -0400

    [TEST] Unittest pipeline

--- pkg/output/structured_test.go
+++ pkg/output/structured_test.go
@@ -9,14 +9,14 @@ import (
 func TestParseToStructured(t *testing.T) {
 	l := `{"log": "info", "time": "2021-08-24T12:41:25-04:00", "msg": "test"}`
 	s := output.ParseToStructured(l)
-	assert.Equal(t, "info", s.Level)
+	assert.Equal(t, "INFO", s.Level)
 	assert.Equal(t, "12:41:25", s.Timestamp)
 	assert.Equal(t, "msg=test", s.Parsed)
 
 
 	l = `{"i": "0", "time": "2021-09-08 15:24:21", "level": "info"}`
 	s = output.ParseToStructured(l)
-	assert.Equal(t, "info", s.Level)
+	assert.Equal(t, "INFO", s.Level)
 	assert.Equal(t, "15:24:21", s.Timestamp)
 	assert.Equal(t, "i=0", s.Parsed)
 }
